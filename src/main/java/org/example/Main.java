5cc9735cd71311ba2589863ef7662167599df8ad
muralic <luckyleo1204@gmail.com>
2023-02-21 11:09:24 +0530

    second push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Again trying to modify ");
+        System.out.println("Again trying to modify");
     }
 }

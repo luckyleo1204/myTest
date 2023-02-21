ced579abe05f19121b4f8c8a165766c7c9c6c705
luckyleo1204 <48104203+luckyleo1204@users.noreply.github.com>
2023-02-21 11:10:07 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Again trying to modify");
+        System.out.println("udpated from directly github"); 
     }
 }

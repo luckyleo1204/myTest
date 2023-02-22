d45e4e28280eca81d1cc2fb18195ae54f36e9b05
muralic <luckyleo1204@gmail.com>
2023-02-22 14:07:38 +0530

    second pushte

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Again trying to modify");
-        System.out.println("udpated from directly github"); 
+        System.out.println("udpated from directly github");;
+        //Test
     }
 }
